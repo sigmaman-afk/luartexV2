e378a555ac7432c16a3a7d7e51d7f7d4f27a8b64
sigmaman-afk <ahnmdjjsvgw2@gmail.com>
2025-12-23 18:02:57 +0100

    Update encrypt.ts

--- engine/encrypt.ts
+++ engine/encrypt.ts
@@ -1,3 +1,6 @@
-export function xor(data: number[], key: number) {
-  return data.map(b => b ^ key);
+// engine/encrypt.ts
+export function enc(str: string) {
+  const k = Math.floor(Math.random()*255)
+  const data = [...str].map(c => c.charCodeAt(0) ^ k)
+  return `{k=${k},d={${data.join(",")}}}`
 }
